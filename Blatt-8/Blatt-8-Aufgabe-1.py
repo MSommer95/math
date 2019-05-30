a4415acef6039ad8637610b6ffb943a993880122
MSommer95 <maximilian.sommer@stud.hshl.de>
2019-05-30 12:44:37 +0200

    commit

---
+++ Blatt-8/Blatt-8-Aufgabe-1.py
@@ -0,0 +1,57 @@
+
+b = 34512423561108014234
+a = 115792089210356248762697446949407573530086143415290314195533631308867097853951
+
+
+werteR = []
+werteQ = []
+werteA = []
+ergebnisse = []
+
+
+def ggt(a, b):
+    x = b
+    y = a % b
+
+    multipli = int(a / b)
+
+    werteR.append(multipli)
+    werteQ.append(y)
+    werteA.append(x)
+    if y == 0:
+        return x
+
+    return ggt(x, y)
+
+
+print('Der ggt(' + str(a) + ',' + str(b) + ') ist: ' + str(ggt(a, b)))
+print(werteQ)
+print(werteR)
+
+
+def erwEuklid(werteQ, werteR, i, x, y):
+
+    yi = werteR[i]
+    ergebnis = x - yi * y
+    ergebnisse.append(ergebnis)
+
+    if i == 0:
+        return True
+
+    return erwEuklid(werteQ, werteR, i-1, y, ergebnis)
+
+
+erwEuklid(werteQ, werteR, len(werteQ)-2, 0, 1)
+
+print(ergebnisse)
+
+print('Der ggt(' + str(a) + ',' + str(b) + ') ist: ' + str(ggt(a, b)) + '=' + str(ergebnisse[len(ergebnisse)-2]) + '*'
+      + str(a) + '+' + str(ergebnisse[len(ergebnisse)-1]) + '*' + str(b))
+
+ergebnis = ergebnisse[len(ergebnisse)-2]
+zwischenErgebnis = ((a * b) % a)
+
+if ergebnis < 0:
+    ergebnis = (ergebnisse[len(ergebnisse)-2] + (a * b)) % a
+
+print('Inverse: ' + str(b) + '^-1 = ' + str(ergebnis) + ' mod ' + str(a))
