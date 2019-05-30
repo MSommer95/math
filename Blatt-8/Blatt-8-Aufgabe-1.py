17fc44314764247fd5337d17cfbdff37c182beaa
MSommer95 <maximilian.sommer@stud.hshl.de>
2019-05-30 21:00:47 +0200

    commit

--- Blatt-8/Blatt-8-Aufgabe-1.py
+++ Blatt-8/Blatt-8-Aufgabe-1.py
@@ -2,62 +2,24 @@
 a = 34512423561108014234
 b = 115792089210356248762697446949407573530086143415290314195533631308867097853951
 
-werteR = []
-werteQ = []
-werteA = []
-ergebnisse = []
+
+def erwEuklid(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, x, y = erwEuklid(b % a, a)
+        return (g, y - (b // a) * x, x)
 
 
-def ggt(a, b):
-    x = b
-    y = a % b
-
-    multipli = int(a / b)
-
-    werteR.append(multipli)
-    werteQ.append(y)
-    werteA.append(x)
-    if y == 0:
-        return x
-
-    return ggt(x, y)
+print(erwEuklid(a, b))
 
 
-print('Der ggt(' + str(a) + ',' + str(b) + ') ist: ' + str(ggt(a, b)))
-print(werteQ)
-print(werteR)
+def mulinv(a, b):
+    g, x, _ = erwEuklid(a, b)
+    if g == 1:
+        return x % b
 
 
-def erwEuklid(werteQ, werteR, i, x, y):
+print(mulinv(a, b))
 
-    yi = werteR[i]
-    ergebnis = x - yi * y
-    ergebnisse.append(ergebnis)
-
-    if i == 0:
-        return True
-
-    return erwEuklid(werteQ, werteR, i-1, y, ergebnis)
-
-
-erwEuklid(werteQ, werteR, len(werteR)-2, 0, 1)
-
-print(ergebnisse)
-
-print('Der ggt(' + str(a) + ',' + str(b) + ') ist: ' + str(ggt(a, b)) + '=' + str(ergebnisse[len(ergebnisse)-2]) + '*'
-      + str(a) + '+' + str(ergebnisse[len(ergebnisse)-1]) + '*' + str(b))
-
-ergebnis = ergebnisse[len(ergebnisse)-2]
-zwischenErgebnis = a * ergebnis % b
-
-if (a * ergebnis % b) != 1:
-    ergebnis = ergebnisse[len(ergebnisse) - 1]
-
-if ergebnis < 0:
-    ergebnis = (ergebnisse[len(ergebnisse)-2] + (a * b)) % a
-
-
-
-print('Inverse: ' + str(b) + '^-1 = ' + str(ergebnis) + ' mod ' + str(a))
-
-
+print((mulinv(a, b) * a) % b)
